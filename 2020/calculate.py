ce0b179559a223f103557a5d2043e12fab7a9d28
tmuytten <thomas.uyttenhove@ugent.be>
2020-02-20 22:18:58 +0100

    qsdqs

--- 2020/calculate.py
+++ 2020/calculate.py
@@ -11,7 +11,10 @@ def calculate(books, libraries, nr_of_days):
 
 		# Sort on metric and select libary
 		libraries_sorted = {k: v for k, v in sorted(libraries.items(), key=lambda l: (l[1], l[0]))}
-		setup_curr_library = libraries_sorted[list(libraries_sorted.keys())[0]]
+		if(len(list(libraries_sorted.keys()))) == 0:
+			return setup_libraries
+		else:
+			setup_curr_library = libraries_sorted[list(libraries_sorted.keys())[0]]
 
 		# Update books
 		nr_of_books = int((min(setup_curr_library.max_active_time, nr_of_days - day) - setup_curr_library.number_of_setup_days) * setup_curr_library.number_of_books_per_day)
