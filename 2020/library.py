80cb81ce7c74cd1d3205508a78f6467ee783b8db
Garben Tanghe <garben.tanghe@gmail.com>
2020-02-20 19:11:43 +0100

    updated skeleton

--- 2020/library.py
+++ 2020/library.py
@@ -1,9 +1,9 @@
 class Library:
 	"""A library"""
 
-	def __init__(self, id, number_of_books_per_day):
+	def __init__(self, id, books, number_of_books_per_day):
 		self.id = id
-		self.books = []
+		self.books = books
 		self.number_of_books_per_day = number_of_books_per_day
 
 	def __str__(self):
--- 2020/main.py
+++ 2020/main.py
@@ -1,7 +1,16 @@
 from submission import read, write
-import Book
-import Library
+from book import Book
+from library import Library
 
-read('in/a_example.txt')
 
-write()
+# main
+if __name__ == '__main__':
+	read('in/a_example.txt')
+	# read('in/b_read_on.txt')
+	# read('in/c_incunabula.txt')
+	# read('in/d_tough_choices.txt')
+	# read('in/e_so_many_books.txt')
+	# read('in/f_libraries_of_the_world.txt')
+
+
+	write()
