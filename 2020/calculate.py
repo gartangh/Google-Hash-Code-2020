8e96e348525bbca2f0abba411ee8f07045dbc09f
Garben Tanghe <garben.tanghe@gmail.com>
2020-02-20 19:56:15 +0100

    added skeleton code for calculate

---
+++ 2020/calculate.py
@@ -0,0 +1,5 @@
+def calculate(books, libraries, nr_of_days):
+	# TODO
+	libraries_to_scan = libraries
+
+	return libraries_to_scan
--- 2020/main.py
+++ 2020/main.py
@@ -1,16 +1,29 @@
 from submission import read, write
 from book import Book
 from library import Library
-
+from calculate import calculate
 
 # main
 if __name__ == '__main__':
-	read('in/a_example.txt')
-	# read('in/b_read_on.txt')
-	# read('in/c_incunabula.txt')
-	# read('in/d_tough_choices.txt')
-	# read('in/e_so_many_books.txt')
-	# read('in/f_libraries_of_the_world.txt')
+	# initialization
+	file_names = [
+		'a_example',
+		# 'b_read_on',
+		# 'c_incunabula',
+		# 'd_tough_choices',
+		# 'e_so_many_books',
+		# 'f_libraries_of_the_world'
+	]
 
+	for file_name in file_names:
+		print(f'Reading {file_name}')
+		books, libraries, nr_of_days = read(f'2020/in/{file_name}.txt')
+		print(f'Read {file_name}')
 
-	write()
+		print(f'Calculating solution for {file_name}')
+		libraries_to_scan = calculate(books, libraries, nr_of_days)
+		print(f'Calculated solution for {file_name}')
+
+		print(f'Writing {file_name}')
+		write(f'2020/in/{file_name}.txt', libraries_to_scan)
+		print(f'Wrote {file_name}')
