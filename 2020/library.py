ca3d198044d885d0428b1fe78b51e484cd018ff4
Garben Tanghe <garben.tanghe@gmail.com>
2020-02-24 14:40:50 +0100

    i quit

--- 2020/library.py
+++ 2020/library.py
@@ -8,28 +8,35 @@ class Library:
 	libraries_to_scan = None
 	time_left = None
 
-	def __init__(self, setup_time, rate, book_ids):
+	def __init__(self, id, setup_time, rate, book_ids):
+		self.id = id
 		self.setup_time = setup_time
 		self.rate = rate
 		self.book_ids = book_ids
-		self.sent_book_ids = []
 		self.score = 0
 
 	def __lt__(self, other):
 		return self.score > other.score
 
 	def update_score(self):
-		if (Library.time_left - self.setup_time) <= 0:
-			# to remove this library from the list later
-			self.sent_book_ids = []
-			return
-
-		# number of books that can still be scanned in the given time frame
+		# number of books that can still be scanned in time left to actually scan
 		books_left = (Library.time_left - self.setup_time) * self.rate
-		# check if limited by the given time frame
-		if books_left < len(self.book_ids):
-			# limited by the given time frame
-			self.score = sum(Library.books[book_id].score for book_id in self.book_ids[:books_left])
+		# remove reference of book to library if cannot be scanned in time
+		if books_left <= 0:
+			for book_id in self.book_ids:
+				Library.books[book_id].library_ids.remove(self.id)
+			self.book_ids = []
+			self.score = 0
+			return
+		elif books_left < len(self.book_ids):
+			for book_id in self.book_ids[books_left+1:]:
+				Library.books[book_id].library_ids.remove(self.id)
+			self.book_ids = self.book_ids[:books_left]
 
-		# all books can be scanned
+		# calculate score of remaining books
 		self.score = sum(Library.books[book_id].score for book_id in self.book_ids) / self.setup_time
+
+		if self.score == 0:
+			for book_id in self.book_ids:
+				Library.books[book_id].library_ids.remove(self.id)
+			self.book_ids = []
--- 2020/main.py
+++ 2020/main.py
@@ -15,7 +15,7 @@ def read(filename):
 		# read in libraries
 		for id in range(nr_of_libraries):
 			# read in library info
-			_, nr_of_setup_days, nr_of_books_per_day = [int(nr) for nr in file.readline().split()]
+			_, time_left, rate = [int(nr) for nr in file.readline().split()]
 			# read in library book ids
 			book_ids = [int(book_id) for book_id in file.readline().split()]
 
@@ -28,7 +28,7 @@ def read(filename):
 			book_ids = [book_id for book_id, _ in sorted(sub_dict.items(), key=lambda book: (book[1], book[0]))]
 
 			# create library object
-			library = Library(nr_of_setup_days, nr_of_books_per_day, book_ids)
+			library = Library(id, time_left, rate, book_ids)
 			library.update_score()
 			Library.libraries[id] = library
 
@@ -36,47 +36,45 @@ def read(filename):
 def write(filename):
 	with open(filename, "w+") as file:
 		file.write(f"{len(Library.libraries_to_scan)}\n")
-		for library_id, library in Library.libraries_to_scan.items():
-			file.write(f"{library_id} {len(library.book_ids)}\n")
-			file.write(" ".join([str(book_id) for book_id in library.book_ids]) + "\n")
+		for library_id, book_ids in Library.libraries_to_scan.items():
+			file.write(f"{library_id} {len(book_ids)}\n")
+			file.write(" ".join([str(book_id) for book_id in book_ids]) + "\n")
 
 
 def calculate():
 	while Library.time_left > 0:
 		# stop when no libraries left
 		if not bool(Library.libraries):
-			return Library.libraries_to_scan
+			return
 
 		# sort libraries by score and select the best library
 		Library.libraries = {library_id: library for library_id, library in
 							 sorted(Library.libraries.items(), key=lambda library: (library[1], library[0]))}
 		current_library_id, current_library = list(Library.libraries.items())[0]
 
-		# send books
-		books_left = max(0, (Library.time_left - current_library.setup_time)) * current_library.rate
-		if books_left < len(current_library.book_ids):
-			current_library.sent_book_ids = current_library.book_ids[:books_left]
-		else:
-			current_library.sent_book_ids = current_library.book_ids
-		Library.libraries_to_scan[current_library_id] = current_library
+		# stop condition if no books can be sent at the beginning
+		if len(current_library.book_ids) == 0:
+			return
 
 		# update time left
 		Library.time_left -= current_library.setup_time
 
-		# remove sent books and all references of libraries to it
-		for book_id in current_library.sent_book_ids:
+		# send books by making deep copy to of book ids to libraries to scanL
+		Library.libraries_to_scan[current_library_id] = [book_id for book_id in current_library.book_ids]
+
+		# remove sent books, all references of libraries to it and the current library
+		for book_id in Library.libraries_to_scan[current_library_id]:
 			for library_id in Library.books[book_id].library_ids:
 				Library.libraries[library_id].book_ids.remove(book_id)
 			del Library.books[book_id]
+		del Library.libraries[current_library_id]
 
-		# remove empty libraries and all references of books to it or update scores
+		# update scores and remove empty libraries (there should be no references of books to it)
 		ids_to_remove = []
 		for library_id, library in Library.libraries.items():
 			library.update_score()
-			if len(library.book_ids) == 0:
+			if library.score == 0:
 				ids_to_remove.append(library_id)
-
-
 		for library_id in ids_to_remove:
 			del Library.libraries[library_id]
 
@@ -85,12 +83,12 @@ def calculate():
 if __name__ == '__main__':
 	# initialization
 	filenames = [
-		# 'a_example',
-		# 'b_read_on',
+		'a_example',
+		'b_read_on',
 		'c_incunabula',
 		# 'd_tough_choices',
-		# 'e_so_many_books',
-		# 'f_libraries_of_the_world'
+		'e_so_many_books',
+		'f_libraries_of_the_world'
 	]
 
 	for filename in filenames:
